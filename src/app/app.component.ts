2c000961b6cc3c8d5dcfacc908f2e92da14be3d4
shaneogrady <shaneogrady1991@gmail.com>
2018-05-27 21:23:32 +0100

    updated file to console log RxJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import * as Rx from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,3 +10,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 }
+
+console.log('RXJS', Rx);
